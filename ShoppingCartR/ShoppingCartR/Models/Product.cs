e91dbd1eed1981d66656192a021d425b0eb7f072
vijuaari <vijuaari2@gmail.com>
2025-06-02 11:22:08 -0500

    model,repository,utility and view changes

---
+++ ShoppingCartR/ShoppingCartR/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCartR.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public string ISBN { get; set; }
+    }
+}
---
+++ ShoppingCartR/ShoppingCartR/Utility/EmailSender.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ShoppingCartR.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string Subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
