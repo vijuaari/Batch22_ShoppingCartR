1fe69b8b87fbd9c4a290fbb17636fdcbe00efe5b
vijuaari <vijuaari2@gmail.com>
2025-06-04 17:43:43 -0500

    6/4/25 changes of program controller, for now create is working but not submitting the given book details like description Animal Farm (originally Animal Farm: A Fairy Story) is a satirical allegorical novella, in the form of a beast fable, by George Orwell, first published in England on 17 August 1945. 12WER45 ... ISDN no and 100 .... price

---
+++ ShoppingCartR/ShoppingCartR/Controllers/ProductController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoppingCartR.Models;
+using ShoppingCartR.Repository;
+using ShoppingCartR.ViewModels;
+
+namespace ShoppingCartR.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            List<Product> ProductList = _unitOfWork.Product.GetAllexpression().ToList();
+                return View(ProductList);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+
+        {
+            ProductVM ProductVM = new ProductVM()
+            {
+                CategoryList = _unitOfWork.Category.GetAllexpression().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.CategoryId.ToString()
+
+                }),
+                    Product = new Product()
+            };
+            return View(ProductVM);
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product Product)
+        {
+            try
+            {
+                if (Product == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                 }
+                if (ModelState.IsValid)
+                {
+                    _unitOfWork.Product.Add(Product);
+                    _unitOfWork.Save();
+                    TempData["Success"] = "Product created Successfully";
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+                }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var checkProduct =_unitOfWork.Product.Get(x=> x.ProductId == id);
+            if (checkProduct == null)
+            { 
+                return NotFound(); 
+            }       
+            return View(checkProduct);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product Product)
+        {
+            try
+            {
+                
+                if (ModelState.IsValid)
+                {
+                    _unitOfWork.Product.Update(Product);
+                    _unitOfWork.Save();
+                    TempData["Success"] = "Product updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var checkProduct = _unitOfWork.Product.Get(x => x.ProductId == id);
+            if (checkProduct == null)
+            {
+                return NotFound();
+            }
+            return View(checkProduct);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost , ActionName ("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Deletepost(int id)
+        {
+            try
+            {
+                var checkProduct = _unitOfWork.Product.Get(x => x.ProductId == id);
+                if (checkProduct == null)
+                {
+                    return NotFound();
+                }
+                _unitOfWork.Product.Remove(checkProduct);
+                _unitOfWork.Save();
+                TempData["Success"] = "Product deleted Successfully";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
